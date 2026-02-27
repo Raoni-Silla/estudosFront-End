85978312ee4a4b14ef4a4ad55cf518392cdb220a
Raoni-Silla <raonisilla@gmail.com>
2026-02-27 16:41:13 -0300

    desafio 08 resolvido

---
+++ javascript/desafios/desafio08/script.js
@@ -0,0 +1,22 @@
+document.querySelector('input#gerar').addEventListener('click', function (){
+
+  let numero = document.querySelector('input#numero')
+  
+  if(numero.value === "" || Number (numero.value) < 0){
+    alert('o campo numero não pode ser nulo')
+    return;
+  }
+
+  let numeroConvertido = Number(numero.value)
+
+  let tabuada = document.getElementById('seltab')
+
+  tabuada.innerHTML = ''
+
+  for(let c = 0; c <= 10 ; c++){
+    let item = document.createElement ('option')
+    item.text = ` ${numeroConvertido} x ${c} = ${numeroConvertido * c}`
+    tabuada.appendChild(item)
+  }
+
+})
