a9586594af9da1251ff33dc2dde5f809b9f9d2bd
Raoni-Silla <raonisilla@gmail.com>
2026-02-27 16:17:41 -0300

    fix:mudando console pra inner

--- javascript/desafios/desafio07/script.js
+++ javascript/desafios/desafio07/script.js
@@ -1,23 +1,26 @@
 document.querySelector("input#contar").addEventListener("click", function () {
   let inicio = document.querySelector("input#inicio");
+  let resposta = document.querySelector("p#resposta");
+
   if (inicio.value.trim() === "") {
-    console.log("O campo inicio esta está vazio ou só tem espaços!");
+    resposta.innerHTML = "O campo início está vazio ou só tem espaços!";
     return;
   }
+
   let fim = document.querySelector("input#fim");
   if (fim.value.trim() === "") {
-    console.log("O campo inicio esta está vazio ou só tem espaços!");
+    resposta.innerHTML = "O campo fim está vazio ou só tem espaços!";
     return;
   }
+
   let passo = document.querySelector("input#passo");
   if (passo.value.trim() === "") {
-    console.log("O campo inicio esta está vazio ou só tem espaços!");
+    resposta.innerHTML = "O campo passo está vazio ou só tem espaços!";
     return;
   } else if (Number(passo.value) <= 0) {
     alert("passo 0, considerando o valor como 1");
     passo.value = 1;
   }
-  let resposta = document.querySelector("p#resposta");
 
   let inicioNumber = Number(inicio.value);
   let fimNumber = Number(fim.value);
@@ -29,10 +32,10 @@ document.querySelector("input#contar").addEventListener("click", function () {
     for (inicioNumber; fimNumber >= inicioNumber; inicioNumber += passoNumber) {
       resposta.innerHTML += `${inicioNumber} 👉`;
     }
-  }else{
+  } else {
     for (inicioNumber; inicioNumber >= fimNumber; inicioNumber -= passoNumber) {
-    resposta.innerHTML += `${inicioNumber} 👉`;
-  }
+      resposta.innerHTML += `${inicioNumber} 👉`;
+    }
   }
 
   resposta.innerHTML += "🏁";
